5c3fbf6378c0f3526f79ce37dbaed26a96ea25d8
Tim <sapfiryt@gmail.com>
2018-09-09 19:34:42 +0300

    init commit

---
+++ src/main/java/com/timirlanyat/odb/OdbApplication.java
@@ -0,0 +1,12 @@
+package com.timirlanyat.odb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdbApplication.class, args);
+	}
+}
